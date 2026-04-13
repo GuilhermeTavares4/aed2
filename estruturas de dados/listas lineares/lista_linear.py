39fa95c1af944d6c02d906ac6c9a629e201263de
GuilhermeTavares4 <guideb23@gmail.com>
2026-04-13 15:50:33 -0300

    continuação lista linear

--- estruturas de dados/listas lineares/lista_linear.py
+++ estruturas de dados/listas lineares/lista_linear.py
@@ -12,29 +12,63 @@ class ListaLinear:
             return None
         return self.vetor[posicao]
     
-
     def vazia(self):
-        pass
+        for item in self.vetor:
+            if item != None:
+                return False
+        return True
 
     def tamanho(self):
         tamanho_lista = self.fim + 1 - self.ini
         return tamanho_lista
-
-    def inserir(self, posicao, dado):
+        
+    def inserir(self, dado, posicao):
         if ((self.ini == 0 and self.fim == self.max - 1) or
-        posicao <= 0 or posicao > self.tamanho()+1):
-            return False    
-        if (self.vazia()):
+        posicao < 0 or posicao > self.max - 1):
+            print('ao da pra inserir')
+            return False
+        
+        
+        if self.vazia():
             self.ini = 0
             self.fim = 0
-        elif (self.fim != self.max - 1):
-            for i in range(self.fim, self.ini + posicao -2, -1):
-                self.vetor[i+1] = self.vetor[i]
-            self.fim = self.fim + 1
+            
         else:
-            for i in range(self.ini, self.ini + posicao - 1):
-                self.vetor[i-1] = self.vetor[i]
-            self.ini = self.ini - 1
-        self.vetor[self.ini + posicao - 1] = dado
+            right_has_slots = True
+            left_has_slots = True
+
+            if self.fim == self.max - 1:
+                right_has_slots = False
+                go_left = True
+
+            if self.ini == 0:
+                left_has_slots = False
+                go_left = False
+
+            if right_has_slots and left_has_slots:
+                elementos_a_direita = self.fim - posicao
+                elementos_a_esquerda = posicao - self.ini
+                if elementos_a_esquerda < elementos_a_direita:
+                    go_left = True
+                else:
+                    go_left = False
+            print(go_left)
+
+
+            if go_left:
+                for i in range(self.ini, posicao - 1):
+                    self.vetor[i - 1] = self.vetor[i]
+                self.ini = self.ini - 1
+            else:
+                for i in range(self.fim + 1, posicao, -1):
+                    self.vetor[i] = self.vetor[i - 1]
+                self.fim = self.fim + 1
+
+        self.vetor[self.ini + posicao] = dado
+
+
+
+
+            
 
         return True
--- estruturas de dados/listas lineares/mod.py
+++ estruturas de dados/listas lineares/mod.py
@@ -1,4 +1,12 @@
 from lista_linear import ListaLinear
 
-lista = ListaLinear(5)
+lista = ListaLinear(6)
+
+
+lista.inserir(2, 1)
+lista.inserir(3, 2)
+print(lista.fim)
+lista.inserir(5, 3)
+print(lista.fim)
+lista.inserir(7, 2)
 print(lista.vetor)
